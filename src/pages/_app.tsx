8dfa700f0dbb2e3e33479674e67453253b4312b6
AnatoliiCubex <anatolii.pyroh.cubex@gmail.com>
2023-05-25 12:21:43 +0300

    base settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,35 @@
-import '@components/styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "../material-ui/theme";
+import createEmotionCache from "../material-ui/createEmotionCache";
+
+import Head from "next/head";
+import { AppProps } from "next/app";
+
+import "@styles/globals.css";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* cssbaseline is something like normalize.css */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
