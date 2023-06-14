9cbf5695dc2233bca73e5831e03a9018127f4c79
AnatoliiCubex <anatolii.pyroh.cubex@gmail.com>
2023-06-14 14:10:53 +0300

    add layout, homepage component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { CacheProvider, EmotionCache } from "@emotion/react";
-import Head from "next/head";
 import { AppProps } from "next/app";
 
 import { ThemeProvider } from "@mui/material/styles";
@@ -9,6 +8,7 @@ import theme from "../material-ui/theme";
 import createEmotionCache from "../material-ui/createEmotionCache";
 
 import "@styles/index.scss";
+import { Layout } from "@components/Layout";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -21,13 +21,12 @@ export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
     <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name='viewport' content='initial-scale=1, width=device-width' />
-      </Head>
       <ThemeProvider theme={theme}>
         {/* cssbaseline is something like normalize.css */}
         <CssBaseline />
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </ThemeProvider>
     </CacheProvider>
   );
