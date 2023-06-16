8ad14d30e5555821a49d4b5751219b74331f35ca
AnatoliiCubex <anatolii.pyroh.cubex@gmail.com>
2023-06-16 10:42:08 +0300

    fixed openAI API key bug

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+require("dotenv").config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/api/gpt.ts
+++ src/pages/api/gpt.ts
@@ -10,18 +10,22 @@ export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse<Data>
 ) {
-  const { question } = request.query;
+  try {
+    const { question } = request.query;
 
-  const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "user",
-        content: question,
-      },
-    ],
-  });
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: question,
+        },
+      ],
+    });
 
-  const response_ = completion.data.choices[0].message;
-  response.status(200).json(response_);
+    const response_ = completion.data.choices[0].message;
+    response.status(200).json(response_);
+  } catch (error) {
+    console.log(error);
+  }
 }
